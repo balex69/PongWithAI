1c4686b8fa5c66f6dee99b568575d020d8bb6555
balex69 <119514045+balex69@users.noreply.github.com>
2022-12-22 18:00:20 +0100

    push

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
         GameFrame PongFrame = new GameFrame();
 
-        // TODO terminer v1 - faire v2 avec controlleur IA sur deuxième raquette (voire même toggle en début de partie pour choisir?)
+        /* TODO terminer v1 - faire v2 avec controlleur IA sur deuxième raquette (voire même toggle en début de partie pour choisir?)
+         * Pour IA et toggle : raquette1 tjrs avec un AL; raquette2 avec toggle au début, si AL = joueur, sinon IA?
+         */
     }
 }
